107374caf6cab41c9ebc274c3514e6ec7ead6a75
Augusto Dantas <aldantas@protonmail.com>
2021-02-16 13:55:37 -0300

    Fix start server script

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 path_to_py4j=<path to py4j jar file>
 classpath="hyflex/*:$path_to_py4j"
 
+mkdir -p "class"
 javac -cp $classpath *.java -d "class"
-java -cp "$classpath:class" HyFlexServer.java
+java -cp "$classpath:class" HyFlexServer
